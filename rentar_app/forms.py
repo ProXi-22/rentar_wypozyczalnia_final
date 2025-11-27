e01255cb86e7b14b98f2bec69a54d5bf0a2a9035
ProXi22 <michalopl15@gmail.com>
2025-11-28 00:22:26 +0100

    Rentar V1.3

--- rentar_app/forms.py
+++ rentar_app/forms.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=100, required=False)
-    last_name = forms.CharField(max_length=100, required=False)
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
 
     class Meta:
         model = User
